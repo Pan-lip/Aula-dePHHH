c95156bc31c4b4630b366890d971c60a956e6ed5
Pan-lip <lunnavartanian@gmail.com>
2025-03-17 22:48:42 -0300

    1 aula de Phyton na facul yey ;)

--- Helo.py
+++ Helo.py
@@ -1,1 +1,19 @@
-print("Helo!")
+# print("Helo!")
+# print("Lunna")
+a = 3
+b = 5
+print(2*a*3*b)
+
+# exercicio 3#
+# num1 = 2
+# num2 = 3
+# num3 = 4
+# soma = num1+num2+num3
+# print(soma)
+num1 = fLoat(input("Numero 1 =")
+num2 = float(input("Numero 2 =")
+num3 = int(input("Numero 3 =")
+soma = num1+num2+num3
+print(soma)
+
+
